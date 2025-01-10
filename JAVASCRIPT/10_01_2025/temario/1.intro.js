80801e3d553910e0253241270c8eb1b2d73c674e
Antonio Rosales Martínez <antoniorosalesmartinez90@gmail.com>
2025-01-10 12:16:43 +0100

    feat basic js

---
+++ JAVASCRIPT/10_01_2025/temario/1.intro.js
@@ -0,0 +1,22 @@
+/* var nombre = "Pepe";
+console.log(nombre);
+nombre = "Paco";
+console.log(nombre);
+
+if (true) {
+  var apellido = "Perez";
+}
+
+console.log(apellido); */
+
+//LET Y CONST
+let nombre = "Pepe";
+nombre = "Paco";
+console.log(nombre);
+
+//SCOPE
+
+if (true) {
+  const apellido = "Perez";
+  console.log(apellido);
+}
---
+++ JAVASCRIPT/10_01_2025/temario/2.tiposdedatos.js
@@ -0,0 +1,13 @@
+const string = 'Esto es un "texto" %E&/%$·&1';
+const number = 1234.56;
+const boolean = false;
+const arrays = ["Manuel", "George"];
+const objeto = {
+  marca: "RK",
+  color: "blanco",
+  precio: 50,
+  bluetooth: true,
+  teclas: ["A", "B", "C"],
+};
+const nulo = null;
+const noDefinido = undefined;
