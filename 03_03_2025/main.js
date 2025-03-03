d4793718e45d51459ca25dea1c4b83c6e3653875
Antonio Rosales Martínez <antoniorosalesmartinez90@gmail.com>
2025-03-03 17:08:09 +0100

    feat dom

---
+++ 03_03_2025/main.js
@@ -0,0 +1,7 @@
+//RECUPERAMOS NODOS
+const titulo = document.querySelector("h1");
+const parrafo = document.querySelector("#parrafo");
+const subtitulos = document.querySelectorAll(".subtitulo");
+
+titulo.id = "titulo";
+titulo.textContent = "Esto es un título modificado por JavaScript"
